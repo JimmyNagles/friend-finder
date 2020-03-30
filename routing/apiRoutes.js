c431791e38a71204962b77b55e1c768e1190ee92
jimmy nagles <jimmynagles@gmail.com>
2020-03-29 22:45:42 -0700

    friends numbers

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -15,26 +15,47 @@ module.exports = function(app) {
 
    console.log("user",user)
 
+   var totalDifference = 0;
+
+   var bestMatch = {
+     name: "",
+     photo: "",
+     friendDifference: 1000
+   };
+   
+   var b = user.answers.map(function(item) {
+      return parseInt(item, 10);
+    });
+   console.log(b)
+ 
+    var sum = b.reduce((a, b) => a + b, 0);
+
+   console.log("Sum of users score " + sum);
+   console.log("Best match friend diff " + bestMatch.friendDifference);
+     console.log("+++++++=================++++++++++");
+
+    for (var i = 0; i < friends.length; i++) {
+      console.log(friends[i].name);
+      console.log(friends[i].answers);
+      var c = friends[i].answers.map(function(item) {
+         return parseInt(item, 10);
+       });
+
+       var total = c.reduce((a, c) => a + c, 0);
+
+  console.log('total', total)
+    
+
+    }
+   
+    console.log(bestMatch);
 
    
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
    friends.push(user)
-
+   console.log('userAdded')
    console.log("friends",friends)
 
   
